a73cad0e22d61437695f4cd665da05a6a62173a3
Hendrik Lösch <loeschhendrik@gmail.com>
2015-11-20 15:50:43 +0100

    Added smoke test

---
+++ PersonManagementTool/PersonManagementTool.SystemTests/StartupTests.cs
@@ -0,0 +1,38 @@
+﻿namespace PersonManagementTool.SystemTests
+{
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class StartupTests
+    {
+        private UIMap map;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        [TestMethod]
+        public void ApplicationMustRunForAtleast_2_Seconds()
+        {
+            this.UIMap.StartApplication();
+
+            Playback.Wait(2000);
+
+            this.UIMap.AssertApplicationExists();
+            this.UIMap.CloseApplication();
+        }
+    }
+}
